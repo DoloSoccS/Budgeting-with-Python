1e07b842e0ded09b85c9046dc302301ddbb95451
DeShawn Houston <dhouston718@yahoo.com>
2023-02-28 23:07:15 -0600

    Initial Comments for app

---
+++ main.py
@@ -0,0 +1,4 @@
+#Budgeting tool that will require all monthly bills statically input
+#Pay schedule frequency will be necessary to allocate funds
+#End result will be amount to leave in account for bills, amount to transfer for savings/debts, and the remaining balance for miscellaneous
+
