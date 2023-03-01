1e8fe7bb7530a05e3e1837ee6e335d81251e7c1e
DeShawn Houston <dhouston718@yahoo.com>
2023-03-01 01:01:57 -0600

    Added functions to separate budget catergories

--- main.py
+++ main.py
@@ -4,21 +4,34 @@
 
 
 class newBudget:
-    def __init__(self, utilities, housing, autoLoan, insurances, studentLoans, debts, other, savings):
+    def __init__(self, utilities, housing, autoLoan, insurances, studentLoans, debts, income):
         self.utilities = utilities
         self.housing = housing
         self.autoLoan = autoLoan
         self.insurances = insurances
         self.studentLoans = studentLoans
-        self.debts = debts
-        self.other = other
-        self.savings = savings
-        self.total = self.utilities + self.housing + self.autoLoan + self.insurances + self.studentLoans + self.debts
-
-    def staticMath(self):
-
-        print("Total of all primary static bills is: " + str(self.total))
+        self.income = income
+        self.debts = debts / 12
+        self.savings = income * 0.25
+        self.livingExpenses = self.utilities + self.housing + self.autoLoan + self.insurances
+        self.debts = self.studentLoans + self.debts
 
 
-myBudget = newBudget(400,500,11,1300,90,15,450,600)
-myBudget.staticMath()    
+    def report(self):
+
+        print("Total of all Living Expenses is: $" + str(self.livingExpenses))
+        print("Total of all debts is: $" + str(self.debts))
+        print("Total Savings commitment is: $" + str(self.savings))
+
+    def standing(self):
+        standing = self.income - (self.livingExpenses + self.debts + self.savings)
+
+        if standing > 0:
+            print("Your remaining budget for this period is: $" + str(standing))
+        else:
+            print("Your remaining budget is: $" + str(standing) + ". The 25%% savings percentage may be too high or your debts are piling up.")    
+
+
+myBudget = newBudget(400,1500,500,230,90,6000, 4500)
+myBudget.report()
+myBudget.standing()
