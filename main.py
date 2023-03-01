fecc8a8f68a9d88aeae0dc0d5aad52c8711239cf
DeShawn Houston <dhouston718@yahoo.com>
2023-03-01 00:33:01 -0600

    Added Initial Class

--- main.py
+++ main.py
@@ -2,3 +2,23 @@
 #Pay schedule frequency will be necessary to allocate funds
 #End result will be amount to leave in account for bills, amount to transfer for savings/debts, and the remaining balance for miscellaneous
 
+
+class newBudget:
+    def __init__(self, utilities, housing, autoLoan, insurances, studentLoans, debts, other, savings):
+        self.utilities = utilities
+        self.housing = housing
+        self.autoLoan = autoLoan
+        self.insurances = insurances
+        self.studentLoans = studentLoans
+        self.debts = debts
+        self.other = other
+        self.savings = savings
+        self.total = self.utilities + self.housing + self.autoLoan + self.insurances + self.studentLoans + self.debts
+
+    def staticMath(self):
+
+        print("Total of all primary static bills is: " + str(self.total))
+
+
+myBudget = newBudget(400,500,11,1300,90,15,450,600)
+myBudget.staticMath()    
