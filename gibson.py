0beb09c84aee240efacf8cb2d3bd457a547091e5
l3LUEPHOENIX <ggibson3@angelo.edu>
2023-03-30 20:13:24 -0500

    nested classes in finance object

--- gibson.py
+++ gibson.py
@@ -1,5 +1,16 @@
 # gibson
 
 class finance:
+    class moneyIN:
+        def __init__(self) -> None:
+            pass
+
+    class moneyOUT:
+        def __init__(self) -> None:
+            pass
+
     def __init__(self):
+        pass
+
+    def report(self):
         pass
